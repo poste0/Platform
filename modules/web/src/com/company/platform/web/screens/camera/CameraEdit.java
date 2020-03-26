18da2e9281686c4e653b3aa5554e6a6dccb5d7aa
poste0 <79372009578@yandex.ru>
2020-03-27 01:57:51 +0400

    Updated camera entity. path field has been added. Added a text area for additional parameters of camera url. Added showing of url on the page. Changed the way to find ip and port.

--- modules/web/src/com/company/platform/web/screens/camera/CameraEdit.java
+++ modules/web/src/com/company/platform/web/screens/camera/CameraEdit.java
@@ -3,13 +3,12 @@ package com.company.platform.web.screens.camera;
 import com.company.platform.service.CameraService;
 import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.UserSessionSource;
-import com.haulmont.cuba.gui.components.PasswordField;
-import com.haulmont.cuba.gui.components.TextField;
-import com.haulmont.cuba.gui.components.ValidationException;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.platform.entity.Camera;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import javax.inject.Inject;
 import java.util.Arrays;
@@ -50,6 +49,17 @@ public class CameraEdit extends StandardEditor<Camera> {
     @Inject
     private TextField<String> cameraNameField;
 
+    @Inject
+    private TextField<String> pathField;
+
+    @Inject
+    private TextArea<String> optionArea;
+
+    private StringBuilder address = new StringBuilder();
+
+    @Inject
+    private Label<String> urlLabelValue;
+
     private Logger logger = LoggerFactory.getLogger(CameraEdit.class);
 
     private static final Consumer<String> FIELD_VALIDATOR = s -> {
@@ -58,14 +68,24 @@ public class CameraEdit extends StandardEditor<Camera> {
         }
     };
 
+    private final Consumer<HasValue.ValueChangeEvent<String>> VALUEEVENT = valueEvent -> {
+        generateUrl();
+        this.urlLabelValue.setValue(this.address.toString());
+    };
+
     private static final String PROTOCOL = "rtsp://";
 
     @Subscribe
     public void onAfterShow(AfterShowEvent event){
         logger.info("After show event start");
-        nameField.addValidator(FIELD_VALIDATOR);
-        passwordField.addValidator(FIELD_VALIDATOR);
         addressField.addValidator(FIELD_VALIDATOR);
+        nameField.addValueChangeListener(VALUEEVENT);
+        passwordField.addValueChangeListener(VALUEEVENT);
+        addressField.addValueChangeListener(VALUEEVENT);
+        portField.addValueChangeListener(VALUEEVENT);
+        pathField.addValueChangeListener(VALUEEVENT);
+        optionArea.addValueChangeListener(VALUEEVENT);
+
         //setFields();
         logger.info("After show event end");
     }
@@ -109,18 +129,6 @@ public class CameraEdit extends StandardEditor<Camera> {
 
     public void onOkButton(){
         logger.info("on ok button start");
-        StringBuilder address = new StringBuilder();
-        address.append(PROTOCOL)
-                .append(nameField.getRawValue())
-                .append(":")
-                .append(passwordField.getValue())
-                .append("@")
-                .append(addressField.getRawValue());
-
-        if(!portField.getRawValue().isEmpty()){
-            address.append(":")
-                    .append(portField.getRawValue());
-        }
 
         Camera camera = getEditedEntity();
         camera.setAddress(address.toString());
@@ -132,5 +140,40 @@ public class CameraEdit extends StandardEditor<Camera> {
 
         close(WINDOW_COMMIT_AND_CLOSE_ACTION);
         logger.info("on ok button end");
+    }
+
+    private void generateUrl(){
+        address.delete(0, address.length());
+
+        address.append(PROTOCOL);
+        appendIfNotEmpty(nameField.getRawValue(), nameField.getRawValue() + ":");
+        appendIfNotEmpty(passwordField.getValue(), passwordField.getValue() + "@");
+        appendIfNotEmpty(addressField.getRawValue());
+        if(!portField.getRawValue().isEmpty()){
+            address.append(":")
+                    .append(portField.getRawValue());
+        }
+
+        address.append("/")
+                .append(pathField.getRawValue());
+
+        if(!StringUtils.isEmpty(optionArea.getRawValue())) {
+            address.append("?");
+            Arrays.asList(optionArea.getRawValue().split("\n")).forEach(option -> {
+                address.append(option)
+                        .append("&");
+            });
+            address.deleteCharAt(address.length() - 1);
+        }
+    }
+
+    private void appendIfNotEmpty(String value, String addedValue){
+        if(!StringUtils.isEmpty(value)){
+            this.address.append(value);
+        }
+    }
+
+    private void appendIfNotEmpty(String value){
+        appendIfNotEmpty(value, value);
     }
 }
