31ecc4d72acd4dfebdeb2ed578b09c5cac0bdf9c
poste0 <79372009578@yandex.ru>
2021-01-18 23:24:55 +0400

    Init cameras

--- modules/front/src/app/home/HomePage.tsx
+++ modules/front/src/app/home/HomePage.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
+import {restServices} from "../../cuba/services";
+import {cubaREST} from "../../index";
 
 class HomePage extends React.Component {
   render() {
+    restServices.platform_CameraService.init(cubaREST)();
     return (
       <div>
         <FormattedMessage id="home.welcome" /> Platform!
